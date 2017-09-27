ae180c7aea19c172ab7248fcb77197f823767697
yuuto onga <yuuto.onnga.23@gmail.com>
2017-09-27 19:45:20 +0900

    first

---
+++ Perceptron.py
@@ -0,0 +1,28 @@
+import numpy as np
+class Perceptorn:
+    def __init__(self,eta,n_iter):
+        self.eta=eta
+        self.n_iter=n_iter
+
+    def fit(self,x,y):
+        """xは標本と特徴の2次元配列
+        yは正解のラベル
+        """
+        self.w_=np.zeros(1+x.shape[1])
+        self.errors_=[]
+
+        for _ in range(self.n_iter):
+            errors=0
+            for xi,target in zip(x,y):
+                update=self.eta*(target-predict(xi))
+                self.w_[1:]+=update*xi
+                self.w_[0]+=update
+                erros+=int(update!=0.0)#誤差があるときだけ加算
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self,x):
+        return np.dot(x,self.w_[1:])+self.w[0]
+    
+    def predict(self,x):   
+        np.where(self.net_input(x)>=0,1,-1)
