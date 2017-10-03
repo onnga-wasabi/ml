77b6e61b25a837608383961579b68b8820e16ff0
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-10-04 08:57:39 +0900

    gero

--- use_colormap.py
+++ use_colormap.py
@@ -7,3 +7,7 @@ def plot_decision_regions(x,y,classifiler,resolution=0.02):
     cmap=ListedColormap(colors[:len(np.unique(y))]#labelの数分だけ
 
     x1_min,x1_max=x[:,0].min()-1,x[:0].max()+1
+    x2_min,x2_max=x[:,1].min()-1,x[:1].max()+1
+    
+    xx1,xx2=np.meshgrid(np.arange(x1_min,x1_max,resolution),np.arange(x2_min,x2_max,resolution))
+    z=classifier.predict(np.array([xx1.ravel(),xx2.ravel()]).T)
