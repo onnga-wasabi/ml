3028f9fc5caa84e7c01bd2e6fb8cdf27921e3d03
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-11-09 21:45:59 +0900

    modified file name

--- ch05/wine.py
+++ ch05/wine.py
@@ -5,12 +5,12 @@ import os
 
 
 def load_wine(test_size=0.3,random_state=0):
-    if 'wine.csv' in os.getcwd():
-        df_wine=pd.read_csv('wine.csv')
+    if 'data_wine.csv' in os.getcwd():
+        df_wine=pd.read_csv('data_wine.csv')
     else:
         df_wine=pd.read_csv(
         'https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',header=None)
-        df_wine.to_csv('wine.csv',index=False)
+        df_wine.to_csv('data_wine.csv',index=False)
 
     x,y=df_wine.iloc[:,1:].values,df_wine.iloc[:,0].values
     x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=0)
