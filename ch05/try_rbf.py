e0d2b8454b643fe80886a78c6709b86aa7824fab
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-11-22 21:59:38 +0900

    rm print

---
+++ ch05/try_rbf.py
@@ -0,0 +1,13 @@
+from wine import load_wine
+from sklearn.datasets import make_moons
+from rbf import rbf_kernel_pca
+
+
+def main():
+    x_train, x_test, y_train, y_test = load_wine()
+    x, y = make_moons()
+    rbf_kernel_pca(x, 1, 1)
+
+
+if __name__ == '__main__':
+    main()
