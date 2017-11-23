c7738f5afcb9ccc479e282e247d3b74fd6c95b5c
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-11-23 15:31:45 +0900

    show data

--- ch05/try_rbf.py
+++ ch05/try_rbf.py
@@ -1,12 +1,26 @@
-from wine import load_wine
 from sklearn.datasets import make_moons
 from rbf import rbf_kernel_pca
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def main():
-    x_train, x_test, y_train, y_test = load_wine()
+    # make data
     x, y = make_moons()
-    x_pca = rbf_kernel_pca(x, 1, 2)
+
+    # rbf_kernel_pca(data,gamma,n components)
+    x_pca = rbf_kernel_pca(x, 15, 2)
+    show_data(x_pca, y)
+
+
+def show_data(x, y):
+    fig = plt.figure()
+    plt.subplot(1, 1, 1)
+    plt.scatter(x[y == 0, 0], x[y == 0, 1], marker='o')
+    plt.scatter(x[y == 1, 0], x[y == 1, 1], marker='^')
+    fig.canvas.manager.window.attributes('-topmost', 1)
+
+    plt.show()
 
 
 if __name__ == '__main__':
