17419d8425bdc468226e318ece5b970e23773b7e
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-11-24 14:41:36 +0900

    add cancer.py

---
+++ ch06/cancer.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sys
+import os
+
+
+def load_cancer():
+    if 'cancer.csv' in os.listdir():
+        df = pd.read_csv('cancer.csv')
+    else:
+        df = pd.read_csv(
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
+        df.to_csv('cancer.csv')
