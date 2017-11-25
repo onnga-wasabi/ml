3e8519563e38d3568c6554d2f0f98be12822aa98
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-11-26 02:14:13 +0900

    return x,y

--- ch06/cancer.py
+++ ch06/cancer.py
@@ -9,3 +9,12 @@ def load_cancer():
         df = pd.read_csv(
             'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
         df.to_csv('cancer.csv')
+
+    # df.info()
+    y = df.iloc[:, 2]
+    x = df.iloc[:, 3:]
+    return x, y
+
+
+if __name__ == '__main__':
+    load_cancer()
