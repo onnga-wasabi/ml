d53e1e4abc306b2a21e1ca64a7b3c4b074e8c1ec
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-11-26 15:46:59 +0900

    show learning curve

--- ch06/lc.py
+++ ch06/lc.py
@@ -8,33 +8,75 @@ from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import learing_curve
+from sklearn.model_selection import learning_curve
+
+import matplotlib.pyplot as plt
 
 
 def main():
     x, y = load_cancer()
-    #print(y[0], '\n', x[0])
-    #print(np.where(y == 'M'))
 
     le = LabelEncoder()
     y = le.fit_transform(y)
-    #print('[M B]:', le.transform(['M', 'B']))
-    #print(np.where(y == 1))
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-    pipe_lr = Pipeline([('scl', StandardScaler()), ('pca', PCA(
-        n_components=2)), ('clr', LogisticRegression())])
+    x_train, x_test, y_train, y_test = \
+        train_test_split(x, y, test_size=0.2)
 
-    # stratified k-fold
-    scores = cross_val_score(estimator=pipe_lr,
-                             X=x_train,
-                             y=y_train,
-                             cv=10,
-                             n_jobs=-1)
-    print('CV Accs:')
-    print('\n'.join(['Fold ' + str('{0:2d}'.format(i + 1)) +
-                     ':' + str(score) for i, score in enumerate(scores)]))
-    print('CV Acc:', np.mean(scores), '+/-', np.std(scores))
+    pipe_lr = Pipeline([('scl', StandardScaler()),
+                        ('clr', LogisticRegression())])
+
+    # learning curve returns (n_unique_ticks),array(n_ticks,n_cv_folds)x2
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator=pipe_lr,
+        X=x_train,
+        y=y_train,
+        train_sizes=np.linspace(0.1, 1, 10),
+        cv=10,
+        n_jobs=-1)
+    # train_sizeはk回の学習の中のそれぞれで使うデータ数を指定できる
+    # ここで指定したデータ数に応じてtrain_sizeも返される
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+    show_learning_curve(
+        train_sizes,
+        train_mean,
+        train_std,
+        test_mean,
+        test_std)
+
+
+def show_learning_curve(
+        train_sizes,
+        train_mean,
+        train_std,
+        test_mean,
+        test_std):
+    fig = plt.figure()
+
+    # show train_accuracy
+    plt.plot(train_sizes, train_mean,
+             color='blue', marker='o', label='train accuracy')
+    plt.fill_between(train_sizes,
+                     train_mean + train_std,
+                     train_mean - train_std,
+                     alpha=0.2,
+                     color='blue')
+
+    # show validation accuracy
+    plt.plot(train_sizes, test_mean,
+             color='green', marker='^', label='validation accuracy')
+    plt.fill_between(train_sizes,
+                     test_mean + test_std,
+                     test_mean - test_std,
+                     alpha=0.2,
+                     color='green')
+    fig.canvas.manager.window.attributes('-topmost', 1)
+    plt.legend(loc='lower right')
+    plt.ylim([0.8, 1.0])
+    plt.show()
+    # plt.pause(2)
 
 
 if __name__ == '__main__':
