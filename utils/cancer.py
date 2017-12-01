50f638ef450eefff3e88b4d55479efc4a5c1237f
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-12-01 13:22:34 +0900

    establish utils

---
+++ utils/cancer.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+
+def load_cancer():
+    if 'cancer.csv' in os.listdir():
+        df = pd.read_csv('cancer.csv')
+    else:
+        df = pd.read_csv(
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
+        df.to_csv('cancer.csv')
+
+    # df.info()
+    y = df.iloc[:, 2]
+    x = df.iloc[:, 3:]
+    return x.values, y.values
+
+
+if __name__ == '__main__':
+    load_cancer()
