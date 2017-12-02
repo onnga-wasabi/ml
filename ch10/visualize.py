a134f9e9f953a3a45a6aeacdbf61e1f989faff65
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-12-02 16:17:19 +0900

    show correlationmatrix

--- ch10/visualize.py
+++ ch10/visualize.py
@@ -8,13 +8,35 @@ sys.path.append(os.pardir)
 from utils.housing import load_housing
 
 
-def main():
-    df = load_housing(path='../utils/')
+def show_scatter(df):
     sns.set(style='whitegrid', context='notebook')
     cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
     sns.pairplot(df[cols])
-    #plt.show()
-    
+    plt.show()
+
+    return 0
+
+
+def show_correlation_matrix(df):
+    sns.set()
+    cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+    cm = np.corrcoef(df[cols].values.T)
+    sns.heatmap(cm,
+                alpha=0.8,
+                center=True,
+                annot=True,
+                xticklabels=cols,
+                yticklabels=cols)
+    plt.show()
+
+    return 0
+
+
+def main():
+    df = load_housing(path='../utils/')
+    # show_scatter(df)
+    show_correlation_matrix(df)
+
     return 0
 
 
