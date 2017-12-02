7bbbd378fc07b644af06a2195001ba212fb8e1ee
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-12-03 01:09:32 +0900

    reshape

---
+++ ch10/regressionline.py
@@ -0,0 +1,35 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from linear import LinearRegressionGD
+import os
+import sys
+sys.path.append(os.pardir)
+from utils.housing import load_housing
+
+
+def make_data(test_size=0.3):
+    df = load_housing('../utils/')
+    df = df[['RM', 'MEDV']]
+    y = df['MEDV'].values
+    x = df['RM'].values
+    sc = StandardScaler()
+    x_std = sc.fit_transform(x.reshape(-1, 1))
+    y_std = sc.fit_transform(y.reshape(-1, 1))
+    return train_test_split(x_std, y_std, test_size=test_size)
+
+
+def main():
+    x_train_std, x_test_std, y_train_std, y_test_std = \
+        make_data()
+    lr = LinearRegressionGD()
+    lr.fit(x_train_std, y_train_std)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
