ee38509ef31082c9756ca3c3a0be880068758a56
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-12-02 09:11:56 +0900

    modified path

--- utils/housing.py
+++ utils/housing.py
@@ -4,11 +4,11 @@ import os
 
 def load_housing(path='./'):
     if 'data/housing.csv' in os.listdir(path):
-        df = pd.read_csv('data/housing.csv')
+        df = pd.read_csv(path + 'data/housing.csv')
     else:
         df = pd.read_csv(
             'https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data', header=None, sep='\s+')
-        df.to_csv('data/housing.csv', index=False)
+        df.to_csv(path + 'data/housing.csv', index=False)
     df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX',
                   'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATO', 'B', 'LSTAT', 'MEDV']
     return df
