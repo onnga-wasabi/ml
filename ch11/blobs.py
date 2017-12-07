84f1e862db2b53a6d2b3657dfa66d2f5a94bc1a4
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-12-07 14:30:55 +0900

    show_scatter

--- ch11/blobs.py
+++ ch11/blobs.py
@@ -1,8 +1,21 @@
 from sklearn.datasets import make_blobs
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
-    x,y=make_blobs()
+    x, y = make_blobs(cluster_std=0.5,
+                      center_box=(-5, 5),
+                      random_state=1)
+    fig = plt.figure()
+    fig.canvas.manager.window.attributes('-topmost', 1)
+    markers = ['o', '^', 'x']
+    colors = ['blue', 'red', 'green']
+    for label, marker, color in zip(np.unique(y), markers, colors):
+        plt.scatter(x[y == label, 0], x[y == label, 1],
+                    marker=marker, color=color)
+    plt.show()
     return 0
 
 
