f417d8d9cc932eab4c8b0aed0dde9d8ae64548db
onnga-wasabi <yuuto.onnga.23@gmail.com>
2017-12-14 10:29:54 +0900

    show result

--- ch12/train_mnist.py
+++ ch12/train_mnist.py
@@ -13,12 +13,20 @@ def main():
     nn = NeuralNetMLP(n_output=10,
                       n_features=x_train.shape[1],
                       n_hidden=50,
-                      epochs=50,
+                      epochs=10,
                       minibatches=10)
     nn.fit(x_train, y_train)
-    fig = plt.figure()
+    y_train_pred = nn.predict(x_train)
+    fig, ax = plt.subplots(4, 4)
     fig.canvas.manager.window.attributes('-topmost', 1)
-    plt.plot(np.arange(0, len(nn.cost_)), nn.cost_)
+    ax = ax.flatten()
+    for i in range(16):
+        ax[i].imshow(x_train[i].reshape(rows, cols),
+                     cmap='gray', interpolation='bilinear')
+        ax[i].set_title(str(y_train[i]) + ' => ' + str(y_train_pred[i]))
+        ax[i].set_xticks([])
+        ax[i].set_yticks([])
+    plt.tight_layout()
     plt.show()
 
     return 0
