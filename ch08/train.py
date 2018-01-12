25baa5e64b7748f1edcf270074eefd17b56af436
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-01-12 16:14:33 +0900

    ugokuyo_

---
+++ ch08/train.py
@@ -0,0 +1,39 @@
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from cleansing import *
+from load_data import load_IMDb
+import nltk
+
+
+def main():
+    x_train = load_clean_data()
+    y_train = load_IMDb()[1]
+    x_test = load_clean_data(kind='test')
+    y_test = load_IMDb(kind='test')[1]
+    print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+    stop = nltk.corpus.stopwords.words('english')
+    tfidf = TfidfVectorizer(lowercase=False)
+    tfidf_lr = Pipeline([('vecs', tfidf), ('clf', LogisticRegression())])
+    param_grid = [{'vecs__ngram_range': [(1, 1)],
+                   'vecs__stop_words': [stop, None],
+                   'vecs__tokenizer': [tokenizer, tokenizer_porter],
+                   'clf__penalty': ['l1', 'l2'],
+                   'clf__C': [1.0, 10, 100]},
+                  {'vecs__ngram_range': [(1, 1)],
+                   'vecs__stop_words': [stop, None],
+                   'vecs__tokenizer': [tokenizer, tokenizer_porter],
+                   'vecs__idf':[False],
+                   'vecs__norm':[False],
+                   'clf__penalty': ['l1', 'l2'],
+                   'clf__C': [1.0, 10, 100]}]
+    gs_tfidf_lr = GridSearchCV(estimator=tfidf_lr,
+                               param_grid=param_grid,
+                               cv=5)
+    gs_tfidf_lr.fit(x_train, y_train)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
